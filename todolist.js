f43a591dd4c857dd248f2a20e8fd393889959904
ngti21md <117225303+ngti21md@users.noreply.github.com>
2023-04-11 22:34:31 +0200

    ss

--- todolist.js
+++ todolist.js
@@ -1,4 +1,28 @@
 const add_button = document.querySelector("#add"),
-      to_do = document.querySelector(".list");
+      input = document.querySelector("#input-box"),
+      todo_list = document.querySelector(".list");
 
+
+
+// reference: https://www.youtube.com/watch?v=-pRg_daFjfk&t=371s
+add_button.addEventListener ("click", function() {
+    let tasks = document.createElement("div");
+    tasks.innerText = input.value;
+    todo_list.appendChild(tasks);
+    input.value = "";
+
+// checkbox & trashcan
+    const emptybox = document.createElement("button"),
+          checkbox = document.createElement("button");
+    
+    emptybox.innerHTML = '<span class="fa fa-square"></span>';
+    checkbox.innerHTML = '<span class="fa fa-square-check"></span>';
+
+    todo_list.appendChild(emptybox);
+    
+    const trashcan = document.createElement("button");
+    trashcan.innerHTML = '<span class="fa-solid fa-trash-can-xmark"></span>';
+    trashcan.classList.add("delete-taks");
+    todo_list.appendChild(trashcan);
+})
 
