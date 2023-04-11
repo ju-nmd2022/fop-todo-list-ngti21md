d95bfc39d573905f59648565c46194c8f9dd87bd
ngti21md <117225303+ngti21md@users.noreply.github.com>
2023-04-12 00:08:16 +0200

    bbbb

--- todolist.js
+++ todolist.js
@@ -1,28 +1,42 @@
 const add_button = document.querySelector("#add"),
       input = document.querySelector("#input-box"),
       todo_list = document.querySelector(".list");
-
+let box; // 0: blank -- 1: check
 
 
 // reference: https://www.youtube.com/watch?v=-pRg_daFjfk&t=371s
 add_button.addEventListener ("click", function() {
-    let tasks = document.createElement("div");
+    const tasks = document.createElement("div");
     tasks.innerText = input.value;
     todo_list.appendChild(tasks);
     input.value = "";
 
-// checkbox & trashcan
-    const emptybox = document.createElement("button"),
-          checkbox = document.createElement("button");
-    
-    emptybox.innerHTML = '<span class="fa fa-square"></span>';
-    checkbox.innerHTML = '<span class="fa fa-square-check"></span>';
+// checkbox 
+    const checkbox = document.createElement("button");
+    checkbox.innerHTML = '<i class="fa-regular fa-square"></i>';
+    checkbox.classList.add("checkbox");
+    todo_list.appendChild(checkbox);
+    box = 0;
 
-    todo_list.appendChild(emptybox);
+  // donetask
+    if (box == 0) {
+        checkbox.addEventListener("click", function() {
+            checkbox.innerHTML = '<i class="fa fa-square-check"></i>';
+            box = 1;
+    })};
+  // undonetask
+    if (box == 1) {
+        checkbox.addEventListener("click", function() {
+            checkbox.innerHTML = '<i class="fa-regular fa-square"></i>';
+            box = 0;
+    })};
     
-    const trashcan = document.createElement("button");
-    trashcan.innerHTML = '<span class="fa-solid fa-trash-can-xmark"></span>';
-    trashcan.classList.add("delete-taks");
-    todo_list.appendChild(trashcan);
+// trashcan
+    const deletemark = document.createElement("button");
+    deletemark.innerHTML = '<i class="fa-solid fa-x"></i>';
+    deletemark.classList.add("delete-tasks");
+    todo_list.appendChild(deletemark);
 })
+
+
 
