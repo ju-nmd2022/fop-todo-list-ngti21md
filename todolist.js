2165c56b46249db93d482f8466bf6f89d0715ac3
ngti21md <117225303+ngti21md@users.noreply.github.com>
2023-04-12 05:09:21 +0200

    ggg

--- todolist.js
+++ todolist.js
@@ -4,18 +4,23 @@ const add_button = document.querySelector("#add"),
 let box; // 0: blank -- 1: check
 
 
-// reference: https://www.youtube.com/watch?v=-pRg_daFjfk&t=371s
 add_button.addEventListener ("click", function() {
-    const tasks = document.createElement("div");
+
+if (input.value != "") {    
+    const tasks = document.createElement("div"),
+          inputvalue = input.value;
     tasks.innerText = input.value;
+    tasks.classList.add("tasklist");
     todo_list.appendChild(tasks);
     input.value = "";
+
+    localStorage.todo_list = inputvalue;
 
 // checkbox 
     const checkbox = document.createElement("button");
     checkbox.innerHTML = '<i class="fa-regular fa-square"></i>';
     checkbox.classList.add("checkbox");
-    todo_list.appendChild(checkbox);
+    tasks.appendChild(checkbox);
     box = 0;
 
   // donetask
@@ -23,20 +28,24 @@ add_button.addEventListener ("click", function() {
         checkbox.addEventListener("click", function() {
             checkbox.innerHTML = '<i class="fa fa-square-check"></i>';
             box = 1;
-    })};
-  // undonetask
-    if (box == 1) {
-        checkbox.addEventListener("click", function() {
-            checkbox.innerHTML = '<i class="fa-regular fa-square"></i>';
-            box = 0;
-    })};
     
-// trashcan
+  // undonetask
+        if (box == 1) {
+            checkbox.addEventListener("click", function() {
+                checkbox.innerHTML = '<i class="fa-regular fa-square"></i>';
+                box = 0;
+        })};
+})};
+
+// delete
     const deletemark = document.createElement("button");
     deletemark.innerHTML = '<i class="fa-solid fa-x"></i>';
     deletemark.classList.add("delete-tasks");
-    todo_list.appendChild(deletemark);
-})
+    tasks.appendChild(deletemark);
 
+    deletemark.addEventListener ("click", function() {
+        tasks.remove();
+    })
+}})
 
 
